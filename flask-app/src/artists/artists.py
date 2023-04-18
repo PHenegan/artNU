deaa24fa3f480da216a4e3084829312da291de53
Jonathan Hong <jonathanhng1117@gmail.com>
2023-04-18 12:47:08 -0400

    Added GET method routes for artists

--- flask-app/src/artists/artists.py
+++ flask-app/src/artists/artists.py
@@ -4,3 +4,34 @@ from src import db
 
 
 artists = Blueprint('artists', __name__)
+
+
+# Get all artists from the db
+@artists.route('/artists', methods=['GET'])
+def get_artists():
+    cursor = db.get_db().cursor()
+    cursor.execute('select firstName, lastName, email from customers')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get artist detail for artist with particular artistID
+@artists.route('/artists/<artistID>', methods=['GET'])
+def get_customer(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from customers where id = {0}'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
